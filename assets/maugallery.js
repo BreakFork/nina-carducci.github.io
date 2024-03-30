f2af57b38711a10127a2684ea86a65dd3dadd4c8
HRVB <h.boulangue@gmail.com>
2024-03-30 14:41:08 +0100

    Retrait de la ligne corrigée - OUBLI

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -240,7 +240,6 @@
         return;
       }
       $(".active-tag").removeClass("active active-tag");
-      $(this).addClass("active-tag");
       $(this).addClass("active active-tag");
 
       var tag = $(this).data("images-toggle");
